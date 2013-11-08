1127c8a630af843573692a51be2cc39f48910b25
Benjamin Croker <benjamin.croker@gmail.com>
2013-11-08 16:53:49 +1000

    Used 1-word ngrams. Removing links / stopwords had no effect

--- user_clx.py
+++ user_clx.py
@@ -21,13 +21,18 @@ d = {"screen_name": [name for (name, data) in grouped],
      "tweet_group": [list(data["tweet_group"])[0] for (name, data) in grouped]
 }
 
-print("Removing stopwords")
-tokenizer = RegexpTokenizer(r'\w+')
-d["tweets_text"] = [" ".join(
-                             [w for w in tokenizer.tokenize(tweets.lower())
-                             if w not in stopwords.words("english")]
-                            )
-                    for tweets in d["tweets_text"]]
+# remove all http links
+# print("Removing links")
+# d["tweets_text"] = [" ".join([w for w in tweets.lower().split(" ") if w[0:4] != "http"])
+#                     for tweets in d["tweets_text"]]
+
+# put the main work in a main loop, so the data can be accessed by importing the file
+# if __name__ == "__main__":
+# print("Removing stopwords")
+# tokenizer = RegexpTokenizer(r'\w+')
+# d["tweets_text"] = [" ".join([w for w in tokenizer.tokenize(tweets.lower())
+#                               if w not in stopwords.words("english")])
+#                   for tweets in d["tweets_text"]]
 df = pd.DataFrame(d)
 
 # filter out finance, since there's only 6 of them
@@ -35,12 +40,13 @@ df = df[df["tweet_group"] != "finance"]
 
 # take a tfidf vectorisation of the text
 tfv = TfidfVectorizer(min_df=3, max_features=None, strip_accents='unicode',
-                      analyzer='word', token_pattern=r'\w{1,}',
-                      decode_error = 'ignore',
-                      ngram_range=(1, 2), use_idf=1, smooth_idf=1,
-                      sublinear_tf=1)
+                    analyzer='word', token_pattern=r'\w{1,}',
+                    decode_error = 'ignore',
+                    ngram_range=(1, 1), use_idf=1, smooth_idf=1,
+                    sublinear_tf=1)
 lm = LogisticRegression()
 
+print("Performing TFIDF transform")
 X = tfv.fit_transform(df["tweets_text"])
 y = np.array(list(df["tweet_group"]))
 
